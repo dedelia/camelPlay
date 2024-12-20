3ae4456c6fcf6db18fa7eb75cab54dd7ddabd46f
deliatristu <delia.tristu@gmail.com>
2024-12-20 19:50:22 +0200

    dsl routing with apache Camel

---
+++ camel-spring-dsl-router/src/main/java/org/dede/com/camel/CamelPlayApplication.java
@@ -0,0 +1,13 @@
+package org.dede.com.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelPlayApplication.class, args);
+	}
+
+}
